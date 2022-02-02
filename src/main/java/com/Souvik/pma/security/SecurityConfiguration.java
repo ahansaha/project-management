086c0000588035aae3c26f27574589eeab7db496
Souvik Saha <souviksaha@Souviks-MacBook-Pro.local>
2022-02-02 22:13:22 +0530

    Adding jdbc security (without database) instead of in-memory security

--- src/main/java/com/Souvik/pma/security/SecurityConfiguration.java
+++ src/main/java/com/Souvik/pma/security/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.Souvik.pma.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,9 +16,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
+		auth.jdbcAuthentication().dataSource(dataSource)
+			.withDefaultSchema()
 			.withUser("myuser")
 				.password("pass")
 				.roles("USER")
@@ -40,7 +47,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 			.antMatchers("/projects/new").hasRole("ADMIN")
 			.antMatchers("/employees/new").hasRole("ADMIN")
+			.antMatchers("/h2_console/**").permitAll()
 			.antMatchers("/").authenticated().and().formLogin();
+		
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
 	}
 	
 }
