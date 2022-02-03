1a2766d1f0a197f423cb27faef182c5384138cc4
Souvik Saha <souviksaha@Souviks-MacBook-Pro.local>
2022-02-03 20:18:30 +0530

    local postGres connection with jdbc security

--- src/main/java/com/Souvik/pma/security/SecurityConfiguration.java
+++ src/main/java/com/Souvik/pma/security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,26 +20,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	DataSource dataSource;
 	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource)
-			.withDefaultSchema()
-			.withUser("myuser")
-				.password("pass")
-				.roles("USER")
-			.and()
-			.withUser("taz")
-				.password("taz2")
-				.roles("USER")
-			.and()
-			.withUser("managerUser")
-				.password("pass3")
-				.roles("ADMIN");
-	}
-	
-	@Bean
-	public PasswordEncoder getPassWordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		auth.jdbcAuthentication()
+			.usersByUsernameQuery("select username, password, enabled from user_accounts where username = ?")
+			.authoritiesByUsernameQuery("select username, role, enabled from user_accounts where username = ?")
+			.dataSource(dataSource)
+			.passwordEncoder(bCryptPasswordEncoder);
 	}
 	
 	//Specifying the things the logged in user can do. (Basically the authorization part of the user).
@@ -46,12 +37,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/projects/new").hasRole("ADMIN")
+			.antMatchers("/projects/save").hasRole("ADMIN")
 			.antMatchers("/employees/new").hasRole("ADMIN")
-			.antMatchers("/h2_console/**").permitAll()
-			.antMatchers("/").authenticated().and().formLogin();
-		
-		http.csrf().disable();
-		http.headers().frameOptions().disable();
+			.antMatchers("/employees/save").hasRole("ADMIN")
+			.antMatchers("/", "/**").permitAll()
+			.and()
+			.formLogin();
 	}
 	
 }
